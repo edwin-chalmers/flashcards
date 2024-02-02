ec42c2888b409c4c9ef2522213e2bec4a22e8bdd
Edwin Chalmers <edwinchalmers@gmail.com>
2024-02-01 19:58:32 -0700

    Clean up card-test with beforeEach, as well as add a timer feature to the game

--- test/card-test.js
+++ test/card-test.js
@@ -13,6 +13,20 @@ const {
 } = require('../src/card');
 
 describe('card', function() {
+  let card1, card2, card3, deck, round, guess1, guess2, guess3
+
+  beforeEach(function() {
+    card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    
+    deck = createDeck([card1, card2, card3]);
+    round = createRound(deck);
+    guess1 = 'sea otter';
+    guess2 = 'appendix';
+    guess3 = 'Fitzgerald';
+  });
+
   it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
@@ -41,22 +55,13 @@ describe('card', function() {
   })
 
   it('should count the cards in a deck', function() {
-    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = countCards([card1, card2, card3]);
 
     expect(deck).to.deep.equal(3)
   });
 
   it('should create a round to organize guesses and records', function() {
-    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
     const round = createRound(deck);
-    // console.log(round)
 
     expect(round.deck).to.be.an('array')
     expect(round.currentCard.id).to.equal(1)
@@ -65,108 +70,50 @@ describe('card', function() {
   });
 
   it('should update the turn count', function() {
-    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
+    const correctGuess = 'sea otter'
 
-    takeTurn(round)
-    expect(round.turns).to.equal(1)
-    takeTurn(round)
+    takeTurn(correctGuess, round)
+    expect(round.turns).to.deep.equal(1)
+    takeTurn(correctGuess, round)
     expect(round.turns).to.equal(2)
   })
 
-  it('should evaluate guesses', function() {
-    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-    const correctGuess = 'sea otter'
-    const incorrectGuess = 'pug'
-
-    expect(takeTurn(round, correctGuess)).to.equal('correct!')
-    expect(takeTurn(round, incorrectGuess)).to.equal('incorrect!')
-  })
-
-  it('stores ids of incorrect guesses', function() {
-    const card1 = createCard(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
+  it('stores ids of guesses', function() {
     const incorrectGuess = 'pug';
 
-    expect(takeTurn(round, incorrectGuess)).to.equal('incorrect!');
-    expect(round.incorrectGuesses).to.deep.equal([2]);
+    expect(takeTurn(incorrectGuess, round)).to.equal('incorrect!');
+    expect(round.incorrectGuesses).to.deep.equal([1]);
   })
   
   it('should progess rounds', function() {
-    const card1 = createCard(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-    const guess1 = 'sea otter';
-    const guess2 = 'appendix';
-    const guess3 = 'Fitzgerald';
-
     expect(round.turns).to.deep.equal(0);
-    expect(takeTurn(round, guess1)).to.equal('correct!');
+    expect(takeTurn(guess1, round)).to.equal('correct!');
     expect(round.incorrectGuesses).to.deep.equal([]);
     expect(round.turns).to.deep.equal(1);
-    expect(takeTurn(round, guess2)).to.equal('incorrect!');
+    expect(takeTurn(guess2, round)).to.equal('incorrect!');
     expect(round.incorrectGuesses).to.deep.equal([14]);
     expect(round.turns).to.deep.equal(2);
-    expect(takeTurn(round, guess3)).to.equal('correct!');
+    expect(takeTurn(guess3, round)).to.equal('correct!');
     expect(round.incorrectGuesses).to.deep.equal([14]);
   })
   
   it('should calculate and return the percentage of correct guesses', function() {
-    const card1 = createCard(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-    const guess1 = 'sea otter';
-    const guess2 = 'appendix';
-    const guess3 = 'Fitzgerald';
-    takeTurn(round, guess1)
-    takeTurn(round, guess2)
-    takeTurn(round, guess3)
+    takeTurn(guess1, round)
+    takeTurn(guess2, round)
+    takeTurn(guess3, round)
     var percentCorrect = calculatePercentCorrect(round)
 
     expect(percentCorrect).to.deep.equal(66);
   })
   
   it('should end the round', function() {
-    const card1 = createCard(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-    const guess1 = 'sea otter';
-    const guess2 = 'appendix';
-    const guess3 = 'Fitzgerald';
-    console.log(round.currentCard.correctAnswer)
-    takeTurn(round, guess1)
-    console.log(round.currentCard.correctAnswer)
-    takeTurn(round, guess2)
-    console.log(round.currentCard.correctAnswer)
-    takeTurn(round, guess3)
+    takeTurn(guess1, round)
+    takeTurn(guess2, round)
+    takeTurn(guess3, round)
     const percentCorrect = calculatePercentCorrect(round)
     const roundEnds = endRound(round, percentCorrect)
 
     expect(roundEnds).to.equal('** Round over! ** You answered 66% of the questions correctly!');
   })
-
-
 
 });
